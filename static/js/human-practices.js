2e05a1c9720b9800ae87de686b366830ce49fa4b
keevin <keevin.hw@gmail.com>
2024-06-30 21:25:15 +0800

    adding the dropdown function for HP pages

---
+++ static/js/human-practices.js
@@ -0,0 +1,58 @@
+var acc = document.getElementsByClassName("greydropdown");
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = '100%';
+        }
+    });
+}
+// ================================================================
+var acc = document.getElementsByClassName("lightgreendropdown");
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+            
+
+        } else {
+            panel.style.maxHeight = '100%';
+        }
+        
+    });
+}
+// ================================================================
+var acc = document.getElementsByClassName("greendropdown");
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+            
+
+        } else {
+            panel.style.maxHeight = '100%';
+        }
+    
+    });
+}
