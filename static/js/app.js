063f24e899b05e7e2df0d583fd2e328d3cd9947e
jocelynweng <jweng0715@gmail.com>
2024-07-13 11:18:30 +0800

    fade in animation & got rid of aos

--- static/js/app.js
+++ static/js/app.js
@@ -40,3 +40,44 @@ const progressBar = () => {
   progressbar.style.width = percentage + "%";
 };
 document.addEventListener("scroll", progressBar);
+
+
+const observerFadeIn = new IntersectionObserver(
+  (items) => {
+    items.forEach((i) => {
+      if (i.isIntersecting) {
+        i.target.classList.add("fade-in");
+      } else {
+        i.target.classList.remove("fade-in");
+      }
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
+
+const fadeInItems = document.querySelectorAll(".to-fade-in");
+fadeInItems.forEach((i) => {
+  observerFadeIn.observe(i);
+});
+
+const observerFadeInFast = new IntersectionObserver(
+  (items) => {
+    items.forEach((i) => {
+      if (i.isIntersecting) {
+        i.target.classList.add("fade-in");
+      } else {
+        i.target.classList.remove("fade-in");
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+const fadeInFastItems = document.querySelectorAll(".to-fade-in-fast");
+fadeInFastItems.forEach((i) => {
+  observerFadeInFast.observe(i);
+});
